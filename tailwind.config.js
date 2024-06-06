8bb390162e2e3c46cd5cef883d5af1316c8a1309
rezaulkarimrk <mrk99rk@gmail.com>
2024-06-06 11:02:06 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ['*'],
+  theme: {
+    // fontFamily:{
+    //   'display': ['oswald']
+    // },
+    // fontSize: {
+    //   'md': ['5rem']
+    // },
+    letterSpacing:{
+      '10px': ['10px']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
